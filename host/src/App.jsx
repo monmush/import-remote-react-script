16da8fd3b0bd6d2f032bc4f1c7f8691223a490df
monmush <omiph001@gmail.com>
2022-01-23 21:57:46 +0700

    lab: implement systemjs-import

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,8 +1,9 @@
+// "remote-umd": "https://filum-assets.sgp1.cdn.digitaloceanspaces.com/remote-component-v17/remote-component.umd.js",
+// "remote-system": "https://filum-assets.sgp1.cdn.digitaloceanspaces.com/remote-component-v17/remote-component.system.js",
+// "remote-es": "https://filum-assets.sgp1.cdn.digitaloceanspaces.com/remote-component-v17/remote-component.es.js"
 import ComponentV17Source from "https://filum-assets.sgp1.cdn.digitaloceanspaces.com/remote-component-v17/remote-component.es.js";
 import "./App.css";
 import { interopDefault } from "./libs/vite";
-
-// console.log("ComponentV17Source", ComponentV17Source);
 
 const ComponentV17 = interopDefault(ComponentV17Source);
 
@@ -12,7 +13,9 @@ function App() {
       <header className="App-header">
         <p>The Host</p>
         <div style={{ display: "flex" }}>
-          <div style={{ width: "50vw" }}>{/* <ComponentV17 /> */}</div>
+          <div style={{ width: "50vw" }}>
+            <ComponentV17 />
+          </div>
         </div>
       </header>
     </div>
--- remote-component-v17/src/main.jsx
+++ remote-component-v17/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+if (import.meta.env.DEV) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
 
 export default App;
